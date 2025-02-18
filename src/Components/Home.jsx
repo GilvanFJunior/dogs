9a20876aba8c9c294e23467ff31c12ccec580ab9
gilvan francisco da silva junior <junior_fran_11@hotmail.com>
2025-02-18 12:00:54 -0300

    28 - Adicionado o Head e Footer com sua estilização

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import Feed from './Feed/Feed';
+import Head from './Helper/Head';
 
 const Home = () => {
   return (
     <section className="container mainContainer">
       <Feed />
+      <Head
+        title="Fotos"
+        description="Home do Site Dogs, com o feed de fotos."
+      />
     </section>
   );
 };
