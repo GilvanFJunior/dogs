c3ee19a7cb0a8ca18e01671551169e6df96e5c3f
gilvan francisco da silva junior <junior_fran_11@hotmail.com>
2025-02-19 10:27:54 -0300

    31 - Ajustes de instalação de pacotes e config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
+import mkcert from 'vite-plugin-mkcert';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
+  server: { https: true }, // Not needed for Vite 5+
+  plugins: [react(), svgr(), mkcert()],
 });
